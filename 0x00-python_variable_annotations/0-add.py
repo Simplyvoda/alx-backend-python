822769c23d14e4f64fc3859a19faf154e756f04d
Vodina  Efem <efem.vodina@gmail.com>
2023-12-07 19:36:08 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,12 @@
-##/usr/bin/env python3
-'''This module returns the sum of 2 float variables
+#!/usr/bin/env python3
+'''
+This module returns the sum of 2 float variables
 '''
 
 
 def add(a: float, b: float) -> float:
-    '''a: parameter of type float
+    '''
+    a: parameter of type float
     b: parameter of type float
     return: type float
     '''
