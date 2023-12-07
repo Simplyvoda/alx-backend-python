0cb3afc1713353e63120711af81ada3253fabdbe
Vodina  Efem <efem.vodina@gmail.com>
2023-12-07 19:43:48 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@ This module contains a function that takes
 2 strings and returns a concatenated string
 '''
 
+
 def concat(str1: str, str2: str) -> str:
     '''
     this function concatenates 2 strings
