a5b831a60510597c6235abcdc4d0863ce588159e
Vodina Efem <efem.vodina@gmail.com>
2023-12-07 18:42:19 +0000

    Task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+This module contains a function that takes
+2 strings and returns a concatenated string
+'''
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    this function concatenates 2 strings
+    '''
+    return str1 + str2
