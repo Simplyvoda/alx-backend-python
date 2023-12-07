6fadd0a9fac3afec71868bec300556d35708d565
Vodina Efem <efem.vodina@gmail.com>
2023-12-07 18:25:54 +0000

    Task 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,4 +4,9 @@ This module returns the sum of 2 float variables
 '''
 
 def add(a: float, b: float) -> float:
+    """
+    a: parameter of type float
+    b: parameter of type float
+    return: type float
+    """
     return a + b
