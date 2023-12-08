8454d97d0e109bd5f76051951725b022ef37086a
Vodina Efem <efem.vodina@gmail.com>
2023-12-08 06:31:54 +0000

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+This module contains a function that takes a float
+and returns string representaion of the float
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    This function returns the
+    string representation of a float
+    '''
+    return __str__(n)
