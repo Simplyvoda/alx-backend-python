643e3a6affd5b77972721d05e6aef508a9eadd61
Simplyvoda <efem.vodina@gmail.com>
2024-01-08 15:59:53 +0000

    Task 4 with pycodestyle compliancy

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,6 +6,8 @@ import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from client import GithubOrgClient
+from typing import Dict
+
 
 class TestGithubOrgClient(unittest.TestCase):
     '''
@@ -18,20 +20,24 @@ class TestGithubOrgClient(unittest.TestCase):
       ("abc", {"payload": False})
     ])
     def test_org(
-        self,
-        test_org,
-        test_payload):
+         self,
+         test_org: str,
+         test_payload: Dict) -> None:
         '''
         This method tests that GithubOrgClient.org
         returns the correct value
         '''
-        # patching the get_json call
+        # Patching the requests.get call within GithubOrgClient instance
         with patch.object(
-          GithubOrgClient, 
+          GithubOrgClient,
           "requests.get"
         ) as mock_req:
+            # Creating a mock response object to mimic the response
             mock_res = Mock()
+            # Mocking the json method of the mock response
+            # to return the test_payload
             mock_res.json = Mock(return_value=test_payload)
+            # creating test instance
             test_obj = GithubOrgClient(test_org)
             result = test_obj.org()
 
@@ -40,4 +46,4 @@ class TestGithubOrgClient(unittest.TestCase):
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
