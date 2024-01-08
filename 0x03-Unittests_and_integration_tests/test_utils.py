77e08049f86049dccb93fcd29413637fab106078
Simplyvoda <efem.vodina@gmail.com>
2024-01-08 10:12:14 +0000

    Task 2 first push

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 Test module for utils.access_nested_map
 '''
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -35,6 +36,33 @@ class TestAccessNestedMap(unittest.TestCase):
         '''
         with self.assertRaises(expected_exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''
+    Class to test get json method
+    '''
+    @parameterized.expand([
+      ("http://example.com"), {"payload": True},
+      ("http://holberton.io"), {"payload": False}
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_req_get):
+        '''
+        Function to test that utils.get_json
+        returns expected result
+        '''
+        mock_res = Mock()
+        # json return value of mock object is test_payload
+        mock_res.json.return_value = test_payload
+
+        # return value or mock reguest is a mock object
+        mock_req_get.return_value = mock_res
+
+        result = get_json(test_url)
+
+        mock_req_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
