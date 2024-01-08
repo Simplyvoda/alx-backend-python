33ec786513ff6aec6131d734532b7ac996c82ef9
Simplyvoda <efem.vodina@gmail.com>
2024-01-08 10:36:58 +0000

    Task 2 second push

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json
+from typing import Dict
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -46,23 +47,23 @@ class TestGetJson(unittest.TestCase):
       ("http://example.com"), {"payload": True},
       ("http://holberton.io"), {"payload": False}
     ])
-    @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_req_get):
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
         '''
         Function to test that utils.get_json
         returns expected result
         '''
-        mock_res = Mock()
-        # json return value of mock object is test_payload
-        mock_res.json.return_value = test_payload
+        with patch("requests.get") as mock_get:
+            mock_res = Mock()  # fake mock object
+            # json return value of mock object is test_payload
+            mock_res.json = Mock(return_value=test_payload)
 
-        # return value or mock reguest is a mock object
-        mock_req_get.return_value = mock_res
+            # return value or mock reguest is a mock object
+            mock_get.return_value = mock_res
 
-        result = get_json(test_url)
+            result = get_json(test_url)
 
-        mock_req_get.assert_called_once_with(test_url)
-        self.assertEqual(result, test_payload)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
